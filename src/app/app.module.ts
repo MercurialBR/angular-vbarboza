60ef0f302c3157a03c0cad99bb0c855a371e9a24
Victor Barboza <63432907+MercurialBR@users.noreply.github.com>
2024-10-22 16:37:31 -0300

    banner-infinite criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { BannerInfiniteComponent } from './components/banner-infinite/banner-infinite.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     BannerComponent,
+    BannerInfiniteComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/banner-infinite/banner-infinite.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-infinite',
+  templateUrl: './banner-infinite.component.html',
+  styleUrls: ['./banner-infinite.component.scss']
+})
+export class BannerInfiniteComponent {
+
+}
